66adad45e4e9ffe6a30f898f1269bfa0daf0f085
wesley <9610848@qq.com>
2024-12-09 09:38:44 +0800

    add doc

--- README.md
+++ README.md
@@ -1,4 +1,53 @@
-# OKXTrade 一个简单的量化交易软件
+# OKXTrade
 
-** 此软件仅用于演示目的，投资有风险，请谨慎使用。 **
+**此软件仅用于演示目的，投资有风险，请谨慎使用。**
 
+一个简单的量化交易软件
+
+## 如何开始
+
+克隆这个仓库
+
+```bash
+git clone https://github.com/asny415/okxtrade.git
+```
+
+安装或升级最新版本的deno，请参考[deno官网](https://deno.com/)
+
+```bash
+curl -fsSL https://deno.land/install.sh | sh
+```
+
+下载你需要的数据
+
+```bash
+deno run --allow-write --unstable-kv --allow-read --allow-net main.ts download -p TON-USDT -r 20240901-20241001 -t 1H
+```
+
+复制并修改自己的投资策略
+
+```bash
+cp doc/strategy/hello.ts ./userdata/strategy/
+```
+
+执行backtesting
+
+```bash
+deno run --allow-write --unstable-kv --allow-read --allow-net main.ts backtesting -p TON-USDT -r 20240901-20241001 --webui
+```
+
+添加并编辑 ./userdata/.env 文件，增加以下环境变量
+>OKX_ACCESSKEY=[在OKX申请的AccessKey]
+>OKX_SECRET=[在OKX申请的SECRET]
+>OKX_PASSPHRASE=[在OKX指定的PASSPHRASE]
+>TG_TOKEN=[Telegram机器人Token]
+>TG_CHATID=[Telegram通知发往何处]
+>HEALTH_PING=[一个好用的心跳网站用于检测你的程序是否存活](https://healthchecks.i)
+
+如果你对一切充满信心，可以开始你的量化之旅了，建议一开始使用小资金量测试.
+
+```bash
+deno run --allow-env --allow-write --unstable-kv --allow-read --allow-net main.ts trade -p TON-USDT -s hello  --webui --verbose=2 --telegram
+```
+
+本项目源自[一个简单的想法](https://t.me/yygqg/25)，感谢[freqtrade](https://www.freqtrade.io/en/stable/)提供灵感.
---
+++ doc/strategy/hello.ts
@@ -0,0 +1,45 @@
+import moment from "https://deno.land/x/momentjs@2.29.1-deno/mod.ts";
+import {
+  Strategy,
+  Wallet,
+  Trade,
+  DataFrame,
+  Signal,
+} from "../../common/strategy.ts";
+import { trade_left } from "../../common/func.ts";
+
+// 购买策略，每12小时定投
+export const strategy: Strategy = {
+  name: "basic",
+  timeframes: [
+    { timeframe: "5m", depth: 1 },
+    { timeframe: "1H", depth: 1 },
+  ],
+  populate_buy_trend(
+    current_time: number,
+    current_price: number,
+    wallet: Wallet,
+    trades: Trade[],
+    _dfs: DataFrame[][]
+  ): Signal {
+    const now = new Date(current_time);
+    const trade_tag = moment(now).format("YYYYMMDDHH");
+    if (now.getMinutes() == 55 && now.getHours() % 12 == 10) {
+      if (trades.filter((t) => t.tag == trade_tag).length > 0) return {};
+    } else {
+      return {};
+    }
+    const open_trades = trades.filter((t) => t.is_open);
+    const total_value =
+      wallet.balance +
+      open_trades.reduce((r, t) => (r += trade_left(t) * t.open_rate), 0);
+    const split_total = 6;
+    const left_rate = wallet.balance / total_value;
+    if (left_rate < 1 / split_total) return {};
+    return {
+      price: current_price,
+      amount: total_value / split_total / current_price,
+      tag: trade_tag,
+    };
+  },
+};
